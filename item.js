f9bc9a7356dcce40729a87b7176bbb9113930f92
Robert Henderson <robert@nterpryz.com>
2018-02-13 15:30:28 +0000

    item has name, price & bogof

--- item.js
+++ item.js
@@ -1,5 +1,8 @@
-const Item = function(name){
-  this.name = name;
+const Item = function(name, price, isBogof){
+  this.name   = name;
+  this.price  = price;
+  this.isBogof = isBogof;
+
 }
 
 module.exports = Item;
--- specs/item_spec.js
+++ specs/item_spec.js
@@ -2,8 +2,20 @@ const assert = require ("assert")
 const Item = require ("../item.js")
 
 describe("Item", function(){
+
+  let shoe;
+
+  beforeEach(function(){
+    shoe = new Item("Trainer", 20, true);
+  });
+
   it("should have a name", function(){
-    const shoe = new Item("Trainer");
     assert.strictEqual(shoe.name, "Trainer")
   });
+  it("should have a price", function(){
+    assert.strictEqual(shoe.price, 20)
+  })
+  it("should state if its BOGOF", function(){
+    assert.strictEqual(shoe.isBogof, true);
+  })
 });
