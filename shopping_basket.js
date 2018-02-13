868531cf76afb0d9d27cddc3ecc38a15c8335f2a
Robert Henderson <robert@nterpryz.com>
2018-02-13 15:48:14 +0000

    can add items to basket

--- shopping_basket.js
+++ shopping_basket.js
@@ -2,4 +2,9 @@ const shopping_basket = function(){
   this.items = [];
 }
 
+shopping_basket.prototype.addItem = function(item){
+  this.items.push(item);
+}
+
+
 module.exports = shopping_basket;
--- specs/shopping_basket_specs.js
+++ specs/shopping_basket_specs.js
@@ -1,14 +1,25 @@
 const assert = require("assert")
 const ShoppingBasket = require ("../shopping_basket.js")
+const Item = require("../item.js")
 
 describe ("basket", function(){
   let basket;
+  let shoe;
+  let baseballBat
+
   beforeEach(function(){
-    basket = new ShoppingBasket();
+    basket  = new ShoppingBasket();
+    shoe    = new Item("Trainer", 20, true);
+    baseballBat = new Item("Nike", 10, false);
   });
 
   it("should start of empty", function(){
     assert.deepStrictEqual(basket.items, []);
   });
 
+  it("should take in items", function(){
+    basket.addItem(shoe);
+    basket.addItem(baseballBat);
+    assert.strictEqual(basket.items.length, 2)
+  })
 });
